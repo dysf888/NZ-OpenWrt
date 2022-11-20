6c00cf00e8b2f1c4e9f943caee6fcfbfcca14686
dysf888 <1808308428@qq.com>
2022-11-20 13:11:26 +0800

    v0.0.1

---
+++ diy.sh
@@ -0,0 +1,6 @@
+sed -i -e 's/OpenWrt/GongQiong/g' -e 's/192.168.1.1/192.168.88.1/g' package/base-files/files/bin/config_generate
+sed -i -e '$s/$/\n 共穷国际 /' -e '$s?$?\n 仓库地址:https://github.com/dysf888/NZ-OpenWrt/ ?' package/base-files/files/etc/banner
+sed -i 's/OpenWrt/OpenWrt 共穷国际 /' package/lean/default-settings/files/zzz-default-settings
+sed -i '/cpuusage/a\                <tr><td width="33%"><%:Compiler author%></td><td>共穷国际</td></tr>' package/lean/autocore/files/arm/index.htm
+echo 'msgid "Compiler author"' >> feeds/luci/modules/luci-base/po/zh-cn/base.po
+echo -e 'msgstr "编译作者"\n' >> feeds/luci/modules/luci-base/po/zh-cn/base.po
