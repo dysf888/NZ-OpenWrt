9d491150570679d6a6f872daa83e1d4045295d12
黑歌-共穷国际大中华区CFO <47450409+dysf888@users.noreply.github.com>
2022-11-21 23:16:25 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -1,4 +1,3 @@
 sed -i -e 's/OpenWrt/GongQiong/g' -e 's/192.168.1.1/192.168.88.1/g' package/base-files/files/bin/config_generate
 sed -i -e '$s/$/\n 共穷国际 /' -e '$s?$?\n 仓库地址:https://github.com/dysf888/NZ-OpenWrt/ ?' package/base-files/files/etc/banner
 sed -i 's/OpenWrt/OpenWrt 共穷国际 /' package/lean/default-settings/files/zzz-default-settings
-
